8a8da729773940b7a117e58e113ca8c7e8634695
adrianowy <adrianoabreu.wy@gmail.com>
2021-03-10 22:04:25 -0400

    módulo docker

--- docker/nodejs/index.js
+++ docker/nodejs/index.js
@@ -3,8 +3,26 @@ const app = express()
 
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Adriano')`;
+
+connection.query(sql);
+connection.end(); 
+
 app.get('/', (req, res) => {
-    res.send('<h1>FullCycle 2.0</h1></br><p>Só funfou depois que instalou o nodemon para reinicar o serviço sozinho após qualquer mudança.</p>')
+    res.send(`
+              <h1>FullCycle 2.0</h1>
+              </br>
+              <p>Só funfou depois que instalou o nodemon para reinicar o serviço sozinho após qualquer mudança.</p>
+            `);
 })
 
 app.listen(port, ()=> {
